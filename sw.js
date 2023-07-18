926c4ce9d677391d623012426282321c47f09822
yexca <yexcano@gmail.com>
2023-07-18 11:56:13 +0800

    修正链接错误

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230705.140708';
+const cacheName = 'chirpy-20230718.115202';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
