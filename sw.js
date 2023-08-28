e2a889502ee3dd95a8f7c2c99d67a15cdd73e150
yexca <yexcano@gmail.com>
2023-08-28 15:58:52 +0800

    new paper

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230720.154034';
+const cacheName = 'chirpy-20230828.155703';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
