7aeff0f707062dad9feab986fcb32e3041010151
yexca <yexcano@gmail.com>
2023-09-05 21:53:01 +0800

    post new paper 装饰器模式

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230903.160026';
+const cacheName = 'chirpy-20230905.214828';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
