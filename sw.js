bc13f101c58fc27cc1d668f5b4429eb26a38b599
yexca <yexcano@gmail.com>
2023-09-07 13:58:28 +0800

    post new page 享元模式

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230906.030601';
+const cacheName = 'chirpy-20230907.135725';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
