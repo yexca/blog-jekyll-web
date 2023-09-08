9ede042fe7677b85755e2c48187ab13fa07388d5
yexca <yexcano@gmail.com>
2023-09-08 12:19:54 +0800

    post new page 代理模式

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230907.135725';
+const cacheName = 'chirpy-20230908.121848';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
