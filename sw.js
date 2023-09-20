935810de939c9add2e95bcf1cb1ba8cf66c10a2b
yexca <yexcano@gmail.com>
2023-09-20 21:26:42 +0800

    修正文章

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230910.152925';
+const cacheName = 'chirpy-20230920.212602';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
