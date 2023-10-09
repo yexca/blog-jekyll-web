5ea335e4dc4ffd3fbc8310841531076c71d152b2
yexca <yexcano@gmail.com>
2023-10-09 17:51:02 +0800

    post new paper 解释器模式

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230920.212602';
+const cacheName = 'chirpy-20231009.175016';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
