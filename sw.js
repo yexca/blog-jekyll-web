c53ea01cd08741b6f53f5bf06d8233666a912c63
yexca <yexcano@gmail.com>
2023-12-21 01:30:29 +0800

    post new page 观察者模式

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20231202.213240';
+const cacheName = 'chirpy-20231221.012509';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
