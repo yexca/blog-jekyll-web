407354fba101ab5c826285fae387fd558beb15f8
yexca <yexcano@gmail.com>
2023-12-26 16:13:49 +0800

    post new page 访问者模式

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20231225.211025';
+const cacheName = 'chirpy-20231226.161253';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
