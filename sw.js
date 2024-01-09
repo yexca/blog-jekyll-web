86679764530ac7c89af588d97de35b6b58e5fe5b
yexca <yexcano@gmail.com>
2024-01-09 21:57:23 +0800

    post new page Docker 创建与推送 H5 镜像

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20231227.161930';
+const cacheName = 'chirpy-20240109.215620';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
