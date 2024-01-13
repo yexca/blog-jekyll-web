7c4651c4fd720e1200dfce495d7053df3c6989c2
yexca <yexcano@gmail.com>
2024-01-14 00:03:41 +0800

    post new page maven 学习

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240110.175942';
+const cacheName = 'chirpy-20240114.000248';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
