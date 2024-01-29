a2b42212cef608f7c99af757928bfc2c191ef3b0
yexca <yexcano@gmail.com>
2024-01-29 18:13:42 +0800

    合并分类

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240129.132857';
+const cacheName = 'chirpy-20240129.181259';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
