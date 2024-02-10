5ba02c2ba72e5a34664f8d6226a62cc41a284f56
yexca <yexcano@gmail.com>
2024-02-10 18:16:14 +0800

    post new page 微信登录后端

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240131.165454';
+const cacheName = 'chirpy-20240210.181523';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
