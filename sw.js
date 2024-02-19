6bcfc5691882e4a07c96a2e6a879149fc667add1
yexca <yexcano@gmail.com>
2024-02-20 06:46:42 +0800

    post new page GoLand 变量与常量

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240219.160304';
+const cacheName = 'chirpy-20240220.064528';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
