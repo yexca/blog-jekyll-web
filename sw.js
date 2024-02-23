0249c76da70a40d4d59a1eff569d05f8a33963ca
yexca <yexcano@gmail.com>
2024-02-23 16:48:29 +0800

    post new page Redis

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240222.055752';
+const cacheName = 'chirpy-20240223.164731';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
