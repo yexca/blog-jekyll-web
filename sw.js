ece65af7879456fbf35fcc2d607b6c657b02162d
yexca <yexcano@gmail.com>
2024-02-25 18:04:28 +0800

    post new page Hi SpringCloud

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240223.164731';
+const cacheName = 'chirpy-20240225.180337';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
