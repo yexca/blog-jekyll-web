6c91514727f44a975f48e0691d3372191b1dc8c5
yexca <yexcano@gmail.com>
2024-02-27 20:04:51 +0800

    post new page GoLand 切片

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240226.214848';
+const cacheName = 'chirpy-20240227.200408';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
