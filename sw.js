49adc75e6a7c9ff4a9e6fafb7774858114d0b2f1
yexca <yexcano@gmail.com>
2024-03-02 17:22:54 +0800

    post new page CSS 选择器

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240301.154010';
+const cacheName = 'chirpy-20240302.172159';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
