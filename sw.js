62c60eced1297ef6d4989df84f2f01a7971d07a9
yexca <yexcano@gmail.com>
2024-04-10 16:25:46 +0800

    post new page CSS 背景属性

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240314.211036';
+const cacheName = 'chirpy-20240410.162447';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
