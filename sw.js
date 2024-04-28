098901baee15d8f7878195cfed19117c01b7ebc6
yexca <yexcano@gmail.com>
2024-04-28 15:54:33 +0800

    Ribbon 负载均衡

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240413.221025';
+const cacheName = 'chirpy-20240428.155348';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
