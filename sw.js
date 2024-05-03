7ab8c61564966e5a65b180ccd024f2c56d38347d
yexca <yexcano@gmail.com>
2024-05-03 23:17:02 +0800

    post new page Feign 远程调用

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240502.224134';
+const cacheName = 'chirpy-20240503.231544';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
