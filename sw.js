81eedf25721f9ddb5f85c9c6623318290f71cc40
yexca <yexcano@gmail.com>
2024-05-05 01:29:55 +0800

    post new page Gateway 服务网关

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240503.231544';
+const cacheName = 'chirpy-20240505.010456';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
