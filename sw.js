d96e6bd4cac34ae4793136c4d52407d3a536c60c
yexca <yexcano@gmail.com>
2024-06-03 18:10:57 +0800

    post new page ElementUI

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240528.163808';
+const cacheName = 'chirpy-20240603.180930';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
