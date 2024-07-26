00cba017299e614dcc9a0b33d049ce3f61c62d3f
yexca <yexcano@gmail.com>
2024-07-26 22:29:06 +0800

    post new page Typecho HTTPS 无法登录访问后台

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240603.180930';
+const cacheName = 'chirpy-20240726.222752';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
