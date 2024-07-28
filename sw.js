f4b920d5625bd4343d6014053b9c4486e81138c6
yexca <yexcano@gmail.com>
2024-07-28 14:33:37 +0800

    post new page 算法 n-皇后问题 回溯法

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240726.222752';
+const cacheName = 'chirpy-20240728.143212';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
