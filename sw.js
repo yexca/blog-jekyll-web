6a6172ae646c7f29eb1f38eefb4d2898718d36fe
yexca <yexcano@gmail.com>
2024-08-16 15:04:56 +0800

    post new page 宝塔面板搭建 ZFile

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240809.102359';
+const cacheName = 'chirpy-20240816.150323';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
