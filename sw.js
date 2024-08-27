639989fbb88574f92d1ea47047117ce68555736b
yexca <yexcano@gmail.com>
2024-08-27 11:06:09 +0800

    post new page 基于 Onedrive 建站 (oneindex)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240823.172237';
+const cacheName = 'chirpy-20240827.110431';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
