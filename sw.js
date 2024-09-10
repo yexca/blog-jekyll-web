f451e11b42cddc9a332c171f68a0c7af55b22da0
yexca <yexcano@gmail.com>
2024-09-10 23:17:26 +0800

    post new page 宝塔面板搭建 CuteOne

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240827.110431';
+const cacheName = 'chirpy-20240910.231602';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
