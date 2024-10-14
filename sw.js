ba50c1d9feb6aa8203357476dccb84701a52ac69
yexca <yexcano@gmail.com>
2024-10-14 23:05:16 +0900

    东京大学大学院理工学 数学 2020 问题一

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241002.214654';
+const cacheName = 'chirpy-20241014.220337';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
