6aa6ca903d83a876acb0d5e8ffae24b248fde7ae
yexca <yexcano@gmail.com>
2024-11-04 00:47:31 +0900

    post new page 2022 SA am2 -01-08

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241028.191734';
+const cacheName = 'chirpy-20241103.233918';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
