b3ed423b67021618bf31b40978c7e7aa20f33f71
yexca <yexcano@gmail.com>
2024-11-10 22:42:15 +0900

    post new page HTML 学习三 - 网页布局

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241108.191754';
+const cacheName = 'chirpy-20241110.214101';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
