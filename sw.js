720cd7351408cbf79b7c2ebf0a746124c427d897
yexca <yexcano@gmail.com>
2024-11-15 21:51:19 +0900

    post new page Vue 学习

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241110.214101';
+const cacheName = 'chirpy-20241115.205015';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
