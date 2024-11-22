c460fbb2743a04cad446257571707684737ea6e7
yexca <yexcano@gmail.com>
2024-11-22 23:12:53 +0900

    post new  page 内网穿透

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241121.161228';
+const cacheName = 'chirpy-20241122.221037';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
