142e50db1f64fd4d154267c1d341f52be3e6af72
yexca <yexcano@gmail.com>
2024-11-23 21:57:20 +0900

    post new page 科学大院理工学 2020-3

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241122.221037';
+const cacheName = 'chirpy-20241123.205537';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
