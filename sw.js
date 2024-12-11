fcfd9a368ac08bb64642e0b49f21919cfa8c49ba
yexca <yexcano@gmail.com>
2024-12-11 20:35:33 +0900

    post new page GoLang 结构体标签

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241203.143149';
+const cacheName = 'chirpy-20241211.193428';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
