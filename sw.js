e679b37bb5f1303fd586a2f1a37f044d8b74bb06
yexca <yexcano@gmail.com>
2024-12-17 22:22:37 +0900

    post new page GoLang goroutine

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20241211.193428';
+const cacheName = 'chirpy-20241217.212041';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
